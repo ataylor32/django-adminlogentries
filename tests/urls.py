6aa99b0444b3747369404bcaee4b50183f8194e0
Adam Taylor <ataylor32@gmail.com>
2021-12-11 14:22:43 -0700

    Added support for Django 4.0

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
 from django.contrib import admin
 
+try:
+    from django.urls import re_path
+except ImportError:
+    # Django 1.11
+    from django.conf.urls import url as re_path
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
 ]
